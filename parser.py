1a49cf70eab5ef53a7fb39e283aa9f166e9ec518
pedram <peeeedi@gmail.com>
2022-01-04 04:31:49 +0330

    modify the name of the variables for readability

--- parser.py
+++ parser.py
@@ -6,14 +6,14 @@ doc = r.text
 
 def html_parser(doc):
     soup = BeautifulSoup(doc, 'html.parser')
-    res0 = soup.find_all('td', 'titleColumn')
-    res1 = soup.find_all('td', 'imdbRating')
+    get_tilte_column = soup.find_all('td', 'titleColumn')
+    get_imdbrate_column = soup.find_all('td', 'imdbRating')
     with open('250', 'w') as data:
-        for i in range(len(res0)):
-            movies_name = res0[i].contents[1].contents[0]
-            movies_number = res0[i].contents[0].strip()
-            movies_year_release = res0[i].contents[3].string
-            movies_rate = res1[i].contents[1]['title']
+        for i in range(len(get_tilte_column)):
+            movies_name = get_tilte_column[i].contents[1].contents[0]
+            movies_number = get_tilte_column[i].contents[0].strip()
+            movies_year_release = get_tilte_column[i].contents[3].string
+            movies_rate = get_imdbrate_column[i].contents[1]['title']
             final_res = '{0:4} {1:70} {2:8} {3:40}'.format(movies_number, movies_name, movies_year_release, movies_rate)
             data.writelines(final_res+'\n')
 
